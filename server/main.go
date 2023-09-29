3c7e74276e0e05be4a3b21fa00b0c25e6377298b
Laxman Giri <kesharigiri000@gmail.com>
2023-09-29 15:33:38 +0545

    start grpc server

--- server/main.go
+++ server/main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/Girilaxman000/go-grpc/proto"
+	"google.golang.org/grpc"
+)
+
+type helloServer struct{
+	pb.GreetServiceServer
+}
+
+func main() {
+	list, err := net.Listen("tcp", ":8000")
+	if err != nil{
+		log.Fatalf("Failed to start the server")
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterGreetServiceServer(grpcServer, &helloServer{})
+	log.Printf("server started at %v", list.Addr())
+	if err := grpcServer.Serve(list); err != nil{
+		log.Fatalf("Failed to start")
+	}
+}
